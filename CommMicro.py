0de075ff1965266b065d98b0a3ee248f5ef15c24
matt <matt@SLACMachine.myguest.virtualbox.org>
2023-10-26 19:01:31 +0100

    getUserVal started migration to model-based pattern

--- CommMicro.py
+++ CommMicro.py
@@ -21,13 +21,15 @@ from functools import partial
 from os import makedirs, path, system
 
 import numpy as np
-import physicselog
+
+# import physicselog
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import (QFileDialog, QWidget)
+from PyQt5.QtWidgets import QFileDialog, QWidget
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
 from pydm import Display
 from scipy.fftpack import fft, fftfreq
+from model import Model
 
 # FFt_math has utility functions
 import FFt_math
@@ -35,13 +37,13 @@ import FFt_math
 BUFFER_LENGTH = 16384
 DEFAULT_SAMPLING_RATE = 2000
 
-LASTPATH = ''
+LASTPATH = ""
 DATA_DIR_PATH = "/u1/lcls/physics/rf_lcls2/microphonics/"
 
 
 class MplCanvas(FigureCanvasQTAgg):
-    """ MPLCanvas is the class for the 'canvas' that plots are drawn on and then mapped to the ui
-        They are Figure format described in matplotlib 2.2 documentation """
+    """MPLCanvas is the class for the 'canvas' that plots are drawn on and then mapped to the ui
+    They are Figure format described in matplotlib 2.2 documentation"""
 
     def __init__(self, parent=None, width=5, height=4, dpi=100):
         fig = Figure(figsize=(width, height), dpi=dpi, tight_layout="true")
@@ -51,10 +53,12 @@ class MplCanvas(FigureCanvasQTAgg):
 
 
 class MicDisp(Display):
-
     def __init__(self, parent=None, args=None, ui_filename="FFT_test.ui"):
         super(MicDisp, self).__init__(parent=parent, args=args, ui_filename=ui_filename)
         self.pathHere = path.dirname(sys.modules[self.__module__].__file__)
+        self.model = Model()
+        self.model.save_root_location = DATA_DIR_PATH
+        self.model.save_location = LASTPATH
 
         def getPath(fileName):
             return path.join(self.pathHere, fileName)
@@ -86,8 +90,8 @@ class MicDisp(Display):
         # fill combo boxes
         for cmid in self.CM_IDs:
             self.ui.CMComboBox.addItem(cmid)
-        self.ui.CavComboBox.addItem('Cavities 1-4')
-        self.ui.CavComboBox.addItem('Cavities 5-8')
+        self.ui.CavComboBox.addItem("Cavities 1-4")
+        self.ui.CavComboBox.addItem("Cavities 5-8")
 
         # start out with cavities 1-4 selected
         self.checkboxes = [self.ui.cb1, self.ui.cb2, self.ui.cb3, self.ui.cb4]
@@ -110,7 +114,8 @@ class MicDisp(Display):
         sampling_rate = DEFAULT_SAMPLING_RATE / decimation_num
         self.ui.label_samplingrate.setNum(sampling_rate)
         self.ui.label_acq_time.setNum(
-            BUFFER_LENGTH * decimation_num * number_of_buffers / DEFAULT_SAMPLING_RATE)
+            BUFFER_LENGTH * decimation_num * number_of_buffers / DEFAULT_SAMPLING_RATE
+        )
 
     def ChangeCav(self):
         #   This function responds to a user changing the cavity combo box
@@ -127,10 +132,12 @@ class MicDisp(Display):
     def FFTPlot(self, bPlot, cavUno):
 
         num_points = len(cavUno)
-        sample_spacing = 1.0 / (DEFAULT_SAMPLING_RATE / int(self.ui.comboBox_decimation.currentText()))
+        sample_spacing = 1.0 / (
+            DEFAULT_SAMPLING_RATE / int(self.ui.comboBox_decimation.currentText())
+        )
         yf1 = fft(cavUno)
-        xf = fftfreq(num_points, sample_spacing)[:num_points // 2]
-        bPlot.axes.plot(xf, 2.0 / num_points * np.abs(yf1[0:num_points // 2]))
+        xf = fftfreq(num_points, sample_spacing)[: num_points // 2]
+        bPlot.axes.plot(xf, 2.0 / num_points * np.abs(yf1[0 : num_points // 2]))
 
     # This function gets info from the GUI, fills out LASTPATH,
     #  and returns liNac, cmNumStr, cavNumA, cavNumB
@@ -139,23 +146,32 @@ class MicDisp(Display):
         # I don't get why I need the global declaration
         global LASTPATH
 
-        # cavNumA/B are '1 2 3 4' with spaces in between for 
+        # cavNumA/B are '1 2 3 4' with spaces in between for
         #  script call
         # cavNumStr is '1234' for filename
         cavNumList = []
-        cavNumStr = ''
+        cavNumStr = ""
 
         # get CM id from comboBox
         cmid = self.ui.CMComboBox.currentText()
+        self.model.user_arguments.cryomodule_id = self.ui.CMComboBox.currentText()
 
         # grab the LxB part
-        linac = cmid.split(':')[1]
-
+        linac = cmid.split(":")[1]
+        self.model.user_arguments.linac = self.model.user_arguments.cryomodule_id.split(
+            ":"
+        )[1]
         # grab the CM number
-        cmNumStr = cmid.split(':')[2]
-
+        cmNumStr = cmid.split(":")[2]
+        self.model.user_arguments.cryomodule = (
+            self.model.user_arguments.cryomodule_id.split(":")[2]
+        )
         # read which rack - 0=A, 1=B
         rack = self.ui.CavComboBox.currentIndex()
+        self.model.user_arguments.rack = self.ui.CavComboBox.currentIndex()
+        self.model.user_arguments.rack_delta = (
+            5 if self.model.user_arguments.rack else 1
+        )
         if rack == 0:
             delta = 1
         else:
@@ -164,19 +180,26 @@ class MicDisp(Display):
         # load up cavNumStr ('1234') and cavNumList (['1','2','3','4'])
         for idx, cb in enumerate(self.checkboxes):
             if cb.isChecked():
+                cav_num = str(idx + self.model.user_arguments.rack_delta)
                 cavNumStr += str(idx + delta)
+                self.model.user_arguments.cavity_number_str += cav_num
                 cavNumList += str(idx + delta)
+                self.model.user_arguments.cavity_number_list += cav_num
 
         # Make the path name to be nice
         #        LASTPATH=DATA_DIR_PATH+'ACCL_'+liNac+'_'+cmNumStr+cavNumStr[0]+'0'
-        LASTPATH = path.join(DATA_DIR_PATH, 'ACCL_' + linac + '_' + cmNumStr + '00')
-
+        LASTPATH = path.join(DATA_DIR_PATH, "ACCL_" + linac + "_" + cmNumStr + "00")
+        self.model.save_location = path.join(
+            self.model.save_root_location, "ACCL_" + linac + "_" + cmNumStr + "00"
+        )
         # get today's date as 2- or 4-char strings
         year = str(self.startd.year)
-        month = '%02d' % self.startd.month
-        day = '%02d' % self.startd.day
+        month = "%02d" % self.startd.month
+        day = "%02d" % self.startd.day
         LASTPATH = path.join(LASTPATH, year, month, day)
-
+        self.model.save_location = path.join(self.model.save_location, year, month, day)
+        print("User Args: ", self.model.user_arguments.__dict__)
+        print("** ", LASTPATH == self.model.save_location)
         return linac, cmNumStr, cavNumStr
 
     # setGOVal is the response to the Get New Measurement button push
@@ -193,11 +216,13 @@ class MicDisp(Display):
         self.ui.label_message.setText("Data acquisition started\n")
         self.ui.label_message.repaint()
 
-        resScrptSrce = "/usr/local/lcls/package/lcls2_llrf/srf/software/res_ctl/res_data_acq.py"
+        resScrptSrce = (
+            "/usr/local/lcls/package/lcls2_llrf/srf/software/res_ctl/res_data_acq.py"
+        )
 
         # made the channel access spec for script call
         rack = self.ui.CavComboBox.currentIndex()
-        AB = 'AB'
+        AB = "AB"
         caCmd = "ca://ACCL:" + linac + ":" + str(cmNumSt) + "00:RES" + AB[rack] + ":"
 
         # LASTPATH in this case ultimately looks like:
@@ -217,27 +242,48 @@ class MicDisp(Display):
         # Go to res_cm##_cav####_c#_yyyymmdd_hhmmss
 
         timestamp = datetime.now().strftime("%Y%m%d" + "_" + "%H%M%S")
-        outFile = 'res_CM' + cmNumSt + '_cav' + cavNumStr + '_c' + str(numbWaveF) + '_' + timestamp
+        outFile = (
+            "res_CM"
+            + cmNumSt
+            + "_cav"
+            + cavNumStr
+            + "_c"
+            + str(numbWaveF)
+            + "_"
+            + timestamp
+        )
         self.filNam = outFile
 
-        cmdList = ['python', resScrptSrce, '-D', str(LASTPATH), '-a', caCmd, '-wsp', decimation_str, '-acav']
+        cmdList = [
+            "python",
+            resScrptSrce,
+            "-D",
+            str(LASTPATH),
+            "-a",
+            caCmd,
+            "-wsp",
+            decimation_str,
+            "-acav",
+        ]
         for cav in cavNumStr:
             cmdList += cav
-        cmdList += ['-ch', 'DF', '-c', numbWaveF, '-F', outFile]
+        cmdList += ["-ch", "DF", "-c", numbWaveF, "-F", outFile]
         print(cmdList)
 
         try:
             self.ui.label_message.setText("Data acquisition started\n")
             self.ui.label_message.repaint()
-            process = subprocess.Popen(cmdList, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            process = subprocess.Popen(
+                cmdList, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+            )
             out, err = process.communicate()
             return_code = process.poll()
-            print('Return code {}'.format(return_code))
+            print("Return code {}".format(return_code))
             out = out.decode(sys.stdin.encoding)
             err = err.decode(sys.stdin.encoding)
-            print('Out: {}'.format(out))
+            print("Out: {}".format(out))
             if len(err) > 0:
-                print('Err: {}'.format(err))
+                print("Err: {}".format(err))
             self.ui.label_message.setText("{}".format(out))
             self.ui.label_message.repaint()
 
@@ -252,23 +298,33 @@ class MicDisp(Display):
                         if path.exists(fname):
                             self.getDataBack(fname, tPlot, bPlot)
                         else:
-                            print('file doesnt exist {}'.format(fname))
+                            print("file doesnt exist {}".format(fname))
                     except:
-                        print('No data file found in {} to make plots from'.format(LASTPATH))
+                        print(
+                            "No data file found in {} to make plots from".format(
+                                LASTPATH
+                            )
+                        )
 
             # unsuccess - if return_code != 0
             else:
-                print('return code is not 0')
+                print("return code is not 0")
 
                 e = subprocess.CalledProcessError(return_code, cmdList, output=out)
                 e.stdout, e.stderr = out, err
                 self.ui.label_message.setText(
-                    "Call to microphonics script failed \nreturn code: {}\nstderr: {}".format(return_code,
-                                                                                              str(e.stderr)))
+                    "Call to microphonics script failed \nreturn code: {}\nstderr: {}".format(
+                        return_code, str(e.stderr)
+                    )
+                )
                 self.ui.label_message.repaint()
-                print('stdout {0} stderr {1} return_code {2}'.format(e.stdout, e.stderr, return_code))
+                print(
+                    "stdout {0} stderr {1} return_code {2}".format(
+                        e.stdout, e.stderr, return_code
+                    )
+                )
         except:
-            print('You are exceptional')
+            print("You are exceptional")
             self.ui.label_message.setText("Call to microphonics script failed \n")
             self.ui.label_message.repaint()
 
@@ -290,14 +346,14 @@ class MicDisp(Display):
 
         # fileDial is fun to say
         fileDial = QFileDialog()
-        fname_tuple = fileDial.getOpenFileName(None, 'Open File?', LASTPATH, '')
-        if fname_tuple[0] != '':
+        fname_tuple = fileDial.getOpenFileName(None, "Open File?", LASTPATH, "")
+        if fname_tuple[0] != "":
             self.getDataBack(fname_tuple[0], tPlot, bPlot)
 
         # get file name for elog entry title
-        fnameParts = fname_tuple[0].split('/')
+        fnameParts = fname_tuple[0].split("/")
         for part in fnameParts:
-            if part.startswith('res'):
+            if part.startswith("res"):
                 self.filNam = part
 
         return ()
@@ -316,10 +372,10 @@ class MicDisp(Display):
             cavDataList = FFt_math.parseCavDat(dFDat)
 
             # figure out cavities from filename for legend
-            fnameParts = fname.split('_')
+            fnameParts = fname.split("_")
             # find the fnamePart that starts with cav
             for part in fnameParts:
-                if part.startswith('cav'):
+                if part.startswith("cav"):
                     cavnums = str(part[3:])
 
             tPlot.axes.cla()
@@ -329,27 +385,26 @@ class MicDisp(Display):
 
             for idx, cavData in enumerate(cavDataList):
                 if len(cavData) > 0:
-                    leGend.append('Cav' + cavnums[idx])
-                    tPlot.axes.hist(cavData, bins=140,
-                                    histtype='step', log='True')
+                    leGend.append("Cav" + cavnums[idx])
+                    tPlot.axes.hist(cavData, bins=140, histtype="step", log="True")
 
-                    leGend2.append('Cav' + cavnums[idx])
+                    leGend2.append("Cav" + cavnums[idx])
                     self.FFTPlot(bPlot, cavData)
 
             # put file name on the plot
-            parts = fname.split('/')
-            tPlot.axes.set_title(parts[-1], loc='left', fontsize='small')
+            parts = fname.split("/")
+            tPlot.axes.set_title(parts[-1], loc="left", fontsize="small")
             # tPlot.axes.set_xlim(-200, 200)
             tPlot.axes.set_ylim(bottom=1)
-            tPlot.axes.set_xlabel('Detune (Hz)')
-            tPlot.axes.set_ylabel('Counts')
+            tPlot.axes.set_xlabel("Detune (Hz)")
+            tPlot.axes.set_ylabel("Counts")
             tPlot.axes.grid(True)
             tPlot.axes.legend(leGend)
             tPlot.draw_idle()
 
             bPlot.axes.set_xlim(0, 150)
-            bPlot.axes.set_xlabel('Frequency (Hz)')
-            bPlot.axes.set_ylabel('Relative Amplitude')
+            bPlot.axes.set_xlabel("Frequency (Hz)")
+            bPlot.axes.set_ylabel("Relative Amplitude")
             bPlot.axes.grid(True)
             bPlot.axes.legend(leGend2)
             bPlot.draw_idle()
@@ -370,9 +425,9 @@ class MicDisp(Display):
     def plotWindow(self):
         screen = QtWidgets.QApplication.primaryScreen()
         screenshot = screen.grabWindow(self.xfDisp.ui.frame.winId())
-        screenshot.save('/tmp/srf_micro.png', 'png')
-        system('convert /tmp/srf_micro.png /tmp/srf_micro.ps')
+        screenshot.save("/tmp/srf_micro.png", "png")
+        system("convert /tmp/srf_micro.png /tmp/srf_micro.ps")
 
-        physicselog.submit_entry("lcls2", "MicrophonicsGui",
-                                 "Microphonics Data " + self.filNam, None,
-                                 "/tmp/srf_micro.ps", None)
+        # physicselog.submit_entry("lcls2", "MicrophonicsGui",
+        #                         "Microphonics Data " + self.filNam, None,
+        #                         "/tmp/srf_micro.ps", None)
---
+++ model.py
@@ -0,0 +1,147 @@
+import os
+from datetime import datetime
+
+
+class UserArgs:
+    def __init__(self):
+        self._cavity_number_str = ""
+        self._cavity_number_list = list()
+        self._cryomodule_str = ""
+        self._cmid = ""
+        self._linac = ""
+        self._rack = -1
+        self._rack_delta = -1
+
+    @property
+    def cavity_number_str(self) -> str:
+        return self._cavity_number_str
+
+    @cavity_number_str.setter
+    def cavity_number_str(self, value: str) -> None:
+        self._cavity_number_str = value
+
+    @property
+    def cavity_number_list(self) -> list:
+        return self._cavity_number_list
+
+    @cavity_number_list.setter
+    def cavity_number_list(self, value: list) -> None:
+        self._cavity_number_list = value
+
+    @property
+    def cryomodule(self) -> str:
+        return self._cryomodule_str
+
+    @cryomodule.setter
+    def cryomodule(self, value: str) -> None:
+        self._cryomodule_str = value
+
+    @property
+    def cryomodule_id(self) -> str:
+        return self._cmid
+
+    @cryomodule_id.setter
+    def cryomodule_id(self, value: str) -> None:
+        self._cmid = value
+
+    @property
+    def linac(self) -> str:
+        return self._linac
+
+    @linac.setter
+    def linac(self, value: str) -> None:
+        self._linac = value
+
+    @property
+    def rack(self) -> int:
+        return self._rack
+
+    @rack.setter
+    def rack(self, value: int) -> None:
+        self._rack = value
+
+    @property
+    def rack_delta(self) -> int:
+        return self._rack_delta
+
+    @rack_delta.setter
+    def rack_delta(self, value: int) -> None:
+        self._rack_delta = value
+
+
+class Model:
+    user_arguments: UserArgs
+
+    def __init__(self):
+        self.name = "model"
+        self.user_arguments = UserArgs()
+        self._save_root_location = ""
+        self._save_location = ""
+        self._start_date = datetime.now()
+
+    @property
+    def cryomodules(self) -> list:
+        # CMID = <SC linac> : <CM ID>
+        return [
+            "ACCL:L0B:01",
+            "ACCL:L1B:02",
+            "ACCL:L1B:03",
+            "ACCL:L1B:H1",
+            "ACCL:L1B:H2",
+            "ACCL:L2B:04",
+            "ACCL:L2B:05",
+            "ACCL:L2B:06",
+            "ACCL:L2B:07",
+            "ACCL:L2B:08",
+            "ACCL:L2B:09",
+            "ACCL:L2B:10",
+            "ACCL:L2B:11",
+            "ACCL:L2B:12",
+            "ACCL:L2B:13",
+            "ACCL:L2B:14",
+            "ACCL:L2B:15",
+            "ACCL:L3B:16",
+            "ACCL:L3B:17",
+            "ACCL:L3B:18",
+            "ACCL:L3B:19",
+            "ACCL:L3B:20",
+            "ACCL:L3B:21",
+            "ACCL:L3B:22",
+            "ACCL:L3B:23",
+            "ACCL:L3B:24",
+            "ACCL:L3B:25",
+            "ACCL:L3B:26",
+            "ACCL:L3B:27",
+            "ACCL:L3B:28",
+            "ACCL:L3B:29",
+            "ACCL:L3B:30",
+            "ACCL:L3B:31",
+            "ACCL:L3B:32",
+            "ACCL:L3B:33",
+            "ACCL:L3B:34",
+            "ACCL:L3B:35",
+        ]
+
+    @property
+    def save_location(self):
+        return self._save_location
+
+    @save_location.setter
+    def save_location(self, path: str):
+        # if not os.path.exists(path):
+            # raise FileNotFoundError(
+                # f"Could not find {path}, save location not updated."
+            # )
+        self._save_location = path
+
+    @property
+    def save_root_location(self):
+        return self._save_root_location
+
+    @save_root_location.setter
+    def save_root_location(self, path: str):
+        #if not os.path.exists(path):
+        #    raise FileNotFoundError(
+        #        f"Could not find {path}, save root location not updated."
+        #    )
+        self._save_root_location = path
